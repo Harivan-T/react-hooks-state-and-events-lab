ef6e30b62075a7bd90482aca4ec2b3ace2ae75a7
Harivan-T <Harivantahir@gmial.com>
2024-03-18 05:34:03 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const appClass = isDarkMode ? 'App dark' : 'App light';
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={toggleDarkMode} id="bt">
+          {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +25,11 @@ function App() {
 }
 
 export default App;
+
+
+
+// In the App component, there is a button for toggling between dark mode and light mode. Using the useState hook, create a state variable in the App component. Then, use that variable to determine if our app should be in dark mode or light mode.
+
+// You will also need to add an event handler to the dark mode button, and update state when the button is clicked.
+
+// The actual functionality of changing our app's theme is handled in CSS, so all you have to do is set up the code to update the className of the div based on your state variable:
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+const [added ,setadd]= useState(0);
+ 
+const appClass = added ? "in-cart" : "";
+
+function addCard() {
+setadd(!added);
+}
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+
+      <button className="add" onClick={addCard}>
+       {added ? 'Add to Cart' : 'Remove From Cart'}
+        </button>
     </li>
   );
 }
 
 export default Item;
+ 
+// Add to Cart
+// In the Item component, when the user clicks the <button> element, the item should be 
+//added to their virtual cart. The way we'll show the user 
+//that the item is in the cart is by changing the className on the <li> element:
+
+// <li className="in-cart">
+// {/* the item is in the cart */}
+// </li>
+
+// <li className="">
+// {/* the item is NOT in the cart */}
+// </li>
+// If the item is not in the cart, the <button> element's text should read "Add to Cart", 
+//and if the item is in the cart, the <button> element's text should read
+// "Remove From Cart". Naturally, you'll also need to add state to the Item component to solve this deliverable!
